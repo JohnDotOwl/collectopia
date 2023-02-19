c7ab450111759297344f52f016d190cf36a52ed7
Jonathan T <jonathantjh@hotmail.com>
2023-02-20 00:19:40 +0800

    Feature : Added Persistent Context (user_data_dir)

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { chromium } from "playwright-extra";
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 chromium.use(StealthPlugin())
 
-chromium.launch({ headless: false }).then(async (browser) => {
-  const page = await browser.newPage();
+const user_data_dir = './assets/user_data_dir';
 
+chromium.launchPersistentContext(user_data_dir, { headless: false }).then(async (browser) => {
+  const page = await browser.newPage();
   await page.goto('https://google.com', { waitUntil: "networkidle" });
   await page.screenshot({ path: 'assets/screenshots/google.png' });
   await browser.close();
